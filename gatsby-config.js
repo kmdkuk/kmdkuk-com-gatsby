b2140d2a70289354796f2cb23ff5610bee76e13c
kouki <kouworld0123@gmail.com>
2020-08-22 13:47:48 +0900

    gatsby new kmdkuk-com-gatsby gatsbyjs/gatsby-starter-hello-world
    typescriptに対応

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  siteMetadata: {
+    title: `kmdkuk's blog`,
+  },
+  /* Your site config here */
+  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-typescript`],
+}
